c001dcfc0dfe00b720ab8652e0fe10a08f3f5271
namratha <marynamratha1995@gmail.com>
2021-04-25 01:59:13 -0400

    Update AccessApiCalls.js

--- uber-frontend/src/components/AccessApiCalls.js
+++ uber-frontend/src/components/AccessApiCalls.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //const url = "http://localhost:5000/app/";
 //const url = `http://${process.env.REACT_APP_IP_ADDRESS}:5000/app/`;
-const url="/";
+const url="http://a749f148b68924502938b98686449a6c-498166786.us-west-2.elb.amazonaws.com";
 
 export const signUp = newUser => {
     return axios
